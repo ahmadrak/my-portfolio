b716d4cc2563eb45572cc81e62db955c01222522
ahmadrak <ahmadrak@users.noreply.github.com>
2026-08-16 18:17:28 +0400

    up

--- lid/projects.ts
+++ lid/projects.ts
@@ -83,7 +83,7 @@ export const projects: Project[] = [
     tech: ["NestJS", "Next.js", "Prisma", "PostgreSQL", "Expo", "React Native"],
     video: [
   "https://www.youtube.com/embed/RdzQ8IAAN_0",
-  "https://youtube.com/shorts/7I9YR1zhAdY?feature=share"
+  "/video/mobileapp.png"
 ],
 
 image: ["/images/dashboard1.png",
